7faf28ebb25df380ebc6cedba55f2cb6e686b6f4
Rufus Russell <rufusrussell@gmail.com>
2020-12-06 21:27:09 +0000

    update day6

--- day6/main.go
+++ day6/main.go
@@ -25,65 +25,64 @@ read_loop:
 
 func main() {
 	in := ReadFromStdIn()
+	groups := [][]string{}
+	groups = append(groups, []string{})
+	for _, line := range in {
+		if line == "" {
+			groups = append(groups, []string{})
+		} else {
+			groups[len(groups)-1] = append(groups[len(groups)-1], line)
+		}
+	}
 
 	fmt.Println("Part 1")
-	groupsPart1 := []map[rune]bool{}
-
-	groupsPart1 = append(groupsPart1, map[rune]bool{})
-	for _, line := range in {
-		activeGroup := groupsPart1[len(groupsPart1)-1]
-
-		if line == "" {
-			groupsPart1 = append(groupsPart1, map[rune]bool{})
-		} else {
+	groupsPart1 := make([]map[rune]bool, len(groups))
+	count := 0
+	for idx, group := range groups {
+		for _, line := range group {
 			for _, character := range line {
-				activeGroup[character] = true
+				if groupsPart1[idx] == nil {
+					groupsPart1[idx] = map[rune]bool{}
+				}
+				groupsPart1[idx][character] = true
 			}
 		}
 	}
-	count := 0
 	for _, group := range groupsPart1 {
 		count += len(group)
 	}
-	fmt.Println(count)
+	fmt.Println(count) // 6549
 
 	fmt.Println("Part 2")
-	groupsPart2 := []map[rune]bool{}
-	passengersInGroup := []map[rune]bool{}
+	groupsPart2 := make([]map[rune]bool, len(groups))
+	for idx, group := range groups {
+		if groupsPart2[idx] == nil {
+			groupsPart2[idx] = map[rune]bool{}
+		}
+		if len(group) == 0 {
+			continue
+		}
 
-	groupsPart2 = append(groupsPart2, map[rune]bool{})
-	for _, line := range in {
-		activeGroup := groupsPart2[len(groupsPart2)-1]
+		first := group[0]
+		for _, character := range first {
+			groupsPart2[idx][character] = true
+		}
+		if len(group) > 1 {
+			rest := group[1:]
 
-		if line == "" {
-			firstPassenger, rest := passengersInGroup[0], passengersInGroup[1:]
-
-		passenger_answer_loop:
-			for question := range firstPassenger {
-				for _, passenger := range rest {
-					if !passenger[question] {
-						continue passenger_answer_loop
+			for k := range groupsPart2[idx] {
+				for _, line := range rest {
+					if !strings.ContainsRune(line, k) {
+						delete(groupsPart2[idx], k)
 					}
 				}
-				activeGroup[question] = true
 			}
-
-			// reset passengers
-			passengersInGroup = []map[rune]bool{}
-			// new group
-			groupsPart2 = append(groupsPart2, map[rune]bool{})
-		} else {
-			passenger := map[rune]bool{}
-			for _, character := range line {
-				passenger[character] = true
-			}
-			passengersInGroup = append(passengersInGroup, passenger)
 		}
 	}
 	count = 0
 	for _, group := range groupsPart2 {
 		count += len(group)
 	}
-	fmt.Println(count)
+	fmt.Println(count) // 3466
 
 }
