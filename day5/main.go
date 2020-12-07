472da8dda561c611ddf47053335d8259dd3a7898
Rufus Russell <rufusrussell@gmail.com>
2020-12-07 08:40:32 +0000

    improve day5 solution

--- day5/main.go
+++ day5/main.go
@@ -5,12 +5,8 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
-)
-
-const (
-	START_ROWS    = 128
-	START_COLUMNS = 8
 )
 
 func ReadFromStdIn() []string {
@@ -29,67 +25,30 @@ read_loop:
 	return lines
 }
 
-func FindRow(rowIdentifier string) int {
-	rangeStart := 1
-	rangeEnd := START_ROWS
-	for _, character := range rowIdentifier {
-		if character == 'F' {
-			rangeEnd -= (rangeEnd-rangeStart)/2 + 1
-		}
-		if character == 'B' {
-			rangeStart += (rangeEnd-rangeStart)/2 + 1
-		}
-	}
-
-	if rowIdentifier[len(rowIdentifier)-1] == 'F' {
-		return rangeStart - 1
-	}
-	return rangeEnd - 1
-}
-
-func FindColumn(columnIdentifier string) int {
-	rangeStart := 1
-	rangeEnd := START_COLUMNS
-	for _, character := range columnIdentifier {
-		if character == 'L' {
-			rangeEnd -= (rangeEnd-rangeStart)/2 + 1
-		}
-		if character == 'R' {
-			rangeStart += (rangeEnd-rangeStart)/2 + 1
-		}
-	}
-
-	if columnIdentifier[len(columnIdentifier)-1] == 'L' {
-		return rangeStart - 1
-	}
-	return rangeEnd - 1
-}
-
 func main() {
 	in := ReadFromStdIn()
 
 	fmt.Println("Part 1")
-	maxSeatId := 0
 	seatIds := []int{}
+
 	for _, line := range in {
-		rowIdentifier := line[0:7]
-		columnIdentifier := line[7:]
 
-		row := FindRow(rowIdentifier)
-		column := FindColumn(columnIdentifier)
-
-		seatId := row*8 + column
-		seatIds = append(seatIds, seatId)
-
-		if seatId > maxSeatId {
-			maxSeatId = seatId
-		}
+		// added afterwards, didnt realise it was binary shift before.
+		f := strings.Map(func(r rune) rune {
+			switch r {
+			case 'F', 'L':
+				return '0'
+			default:
+				return '1'
+			}
+		}, line)
+		seatId, _ := strconv.ParseInt(f, 2, 64)
+		seatIds = append(seatIds, int(seatId))
 	}
-	fmt.Println(maxSeatId)
+	sort.Ints(seatIds)
+	fmt.Println(seatIds[len(seatIds)-1])
 
 	fmt.Println("Part 2")
-
-	sort.Ints(seatIds)
 
 	for idx, seatId := range seatIds {
 		if idx != 0 && idx != len(seatIds)-1 {
