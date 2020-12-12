ed14c1680ac63d8c4034a48496078e47bb964437
Rufus Russell <rufusrussell@gmail.com>
2020-12-12 18:01:09 +0000

    solve 2018 day 23

--- 2018/23/main.go
+++ 2018/23/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	// in := ReadFromInput()
-	in := ReadFromStdIn()
+	in := ReadFromInput()
 	bots := []*Bot{}
 
 	fmt.Println("Part 1")
@@ -32,7 +30,47 @@ func main() {
 	fmt.Println(inRange)
 
 	fmt.Println("Part 2")
+	currentCoord := Coord{}
+	currentBestInRange := 0
+	n := []int{-1, 0, 1}
+	multiplier := 1
+	multiplierFactor := 100000000
 
+	startTime := time.Now()
+main_loop:
+	for {
+		nBelow := 0
+		for _, offsetX := range n {
+			for _, offsetY := range n {
+				for _, offsetZ := range n {
+					newCoord := Coord{currentCoord.X + multiplier*offsetX, currentCoord.Y + multiplier*offsetY, currentCoord.Z + multiplier*offsetZ}
+					inRange := calculateNInRange(newCoord, bots)
+					if inRange < currentBestInRange {
+						nBelow++
+					} else if inRange > currentBestInRange {
+						currentCoord = newCoord
+						currentBestInRange = inRange
+						multiplier = 1
+						continue main_loop
+					}
+				}
+			}
+		}
+		// if everything in every direction is less.
+		if nBelow == 26 {
+			if multiplierFactor == 1 {
+				break
+			}
+			multiplierFactor /= 10
+			multiplier = 1
+		} else {
+			multiplier += multiplierFactor
+		}
+	}
+	endTime := time.Now()
+
+	fmt.Println(abs(currentCoord.X) + abs(currentCoord.Y) + abs(currentCoord.Z)) // 160646364
+	fmt.Println("Part 2 took", endTime.Sub(startTime))
 }
 
 type Coord struct {
@@ -42,6 +80,15 @@ type Coord struct {
 type Bot struct {
 	Coord
 	R int
+}
+
+func calculateNInRange(c Coord, bots []*Bot) (total int) {
+	for _, b := range bots {
+		if b.inRange(c) {
+			total++
+		}
+	}
+	return
 }
 
 func (b *Bot) inRange(c Coord) bool {
@@ -68,22 +115,6 @@ func lineToBot(line string) *Bot {
 	z, _ := strconv.Atoi(ints[2])
 	r, _ := strconv.Atoi(split[2])
 	return &Bot{Coord: Coord{x, y, z}, R: r}
-}
-
-func ReadFromStdIn() []string {
-	lines := []string{}
-	reader := bufio.NewReader(os.Stdin)
-
-read_loop:
-	for {
-		text, _ := reader.ReadString('\n')
-		if text == "\n" {
-			break read_loop
-		}
-		lines = append(lines, strings.TrimSpace(text))
-	}
-
-	return lines
 }
 
 func ReadFromInput() []string {
