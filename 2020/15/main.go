e5829ee4493146423af45ca69abd09a9badbd739
Rufus Russell <rufusrussell@gmail.com>
2020-12-15 09:13:37 +0000

    day 15 part 2

--- 2020/15/main.go
+++ 2020/15/main.go
@@ -1,65 +1,42 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	// in := ReadFromInput()
-	in := ReadFromStdIn()
+	in := ReadFromInput()
 
-	fmt.Println("Part 1")
-	turnHistory := StringsToInts(strings.Split(in[0], ","))
-
-	fmt.Println(PlayGame(turnHistory, 2020, map[int]int{}))
-
-	fmt.Println("Part 2")
+	initialState := StringsToInts(strings.Split(in[0], ","))
+	mem := map[int]int{}
+	for idx, turn := range initialState {
+		mem[turn] = idx
+	}
+	PlayGame(initialState, 2020, 30000000, mem)
 
 }
 
-func PlayGame(turnHistory []int, targetTurn int, mem map[int]int) int {
-	for len(turnHistory) < targetTurn {
-		lastSpoken := turnHistory[len(turnHistory)-1]
-		turnsMentioned := FindTurns(turnHistory, lastSpoken)
-		if len(turnsMentioned) == 1 {
-			turnHistory = append(turnHistory, 0)
+func PlayGame(turnHistory []int, targetTurnPart1, targetTurnPart2 int, mem map[int]int) {
+	for len(turnHistory) < targetTurnPart2 {
+		lastTurn := len(turnHistory) - 1
+		lastSpoken := turnHistory[lastTurn]
+		previousTurnSameNumber, exists := mem[lastSpoken]
+		if exists {
+			diff := lastTurn - previousTurnSameNumber
+			mem[lastSpoken] = len(turnHistory) - 1
+			turnHistory = append(turnHistory, diff)
 		} else {
-			turnHistory = append(turnHistory, turnsMentioned[len(turnsMentioned)-1]-turnsMentioned[len(turnsMentioned)-2])
+			mem[lastSpoken] = lastTurn
+			turnHistory = append(turnHistory, 0)
+		}
+		if len(turnHistory) == targetTurnPart1 {
+			fmt.Println("Part 1:", turnHistory[targetTurnPart1-1])
 		}
 	}
-	return turnHistory[targetTurn-1]
-}
-
-func FindTurns(ints []int, intToFind int) []int {
-	foundIndex := []int{}
-
-	for idx, i := range ints {
-		if i == intToFind {
-			foundIndex = append(foundIndex, idx+1)
-		}
-	}
-	return foundIndex
-}
-
-func ReadFromStdIn() []string {
-	lines := []string{}
-	reader := bufio.NewReader(os.Stdin)
-
-read_loop:
-	for {
-		text, _ := reader.ReadString('\n')
-		if text == "\n" {
-			break read_loop
-		}
-		lines = append(lines, strings.TrimSpace(text))
-	}
-
-	return lines
+	fmt.Println("Part 2:", turnHistory[targetTurnPart2-1])
 }
 
 func StringsToInts(stringInputs []string) []int {
