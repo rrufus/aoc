8b35593065c2fc213d29209a19060eee39c13fe4
Rufus Russell <rufusrussell@gmail.com>
2020-12-19 18:06:47 +0000

    part 2

--- 2020/19/main.go
+++ 2020/19/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -13,20 +11,36 @@ import (
 
 func main() {
 	in := ReadFromInput()
-	// in := ReadFromStdIn()
 
 	rules, messages := Parse(in)
-	matcher := regexp.MustCompile(GenerateRegexpString(rules, 0))
-	totalMatched := 0
+	matcher := regexp.MustCompile(GenerateRegexpString(rules, 0, true, false))
+	fmt.Println("Part 1:", CountMatching(matcher, messages))
+
+	rules[8] = "42 | 42 8"
+	rules[11] = "42 31 | 42 11 31"
+	matcherp2String := GenerateRegexpString(rules, 0, true, true)
+	matcherp2 := regexp.MustCompile(matcherp2String)
+	fmt.Println("Part 2:", CountMatching(matcherp2, messages))
+}
+
+func CountMatching(r *regexp.Regexp, messages []string) (totalMatched int) {
 	for _, message := range messages {
-		if matcher.MatchString(message) {
+		if r.MatchString(message) {
 			totalMatched++
 		}
 	}
-	fmt.Println("Part 1:", totalMatched)
+	return
 }
 
-func GenerateRegexpString(rulesMap map[int]string, ruleNumber int) string {
+func GenerateRegexpUpToNBeforeAndAfter(before string, after string, n int) string {
+	possibilities := []string{}
+	for i := 0; i < n; i++ {
+		possibilities = append(possibilities, fmt.Sprintf("%v{%v}%v{%v}", before, i+1, after, i+1))
+	}
+	return "(" + strings.Join(possibilities, "|") + ")"
+}
+
+func GenerateRegexpString(rulesMap map[int]string, ruleNumber int, includeStartAndEnd bool, part2 bool) string {
 	currentRule := rulesMap[ruleNumber]
 
 	if currentRule == "a" || currentRule == "b" {
@@ -35,15 +49,31 @@ func GenerateRegexpString(rulesMap map[int]string, ruleNumber int) string {
 
 	matchingString := []string{}
 
-	for _, part := range strings.Split(currentRule, " | ") {
-		stringPart := ""
-		for _, ruleNumber := range StringsToInts(strings.Split(part, " ")) {
-			stringPart += strings.Trim(GenerateRegexpString(rulesMap, ruleNumber), "$^")
+	if part2 && ruleNumber == 8 {
+		matchingString = append(matchingString, GenerateRegexpString(rulesMap, 42, false, part2)+"+")
+	} else if part2 && ruleNumber == 11 {
+		before := GenerateRegexpString(rulesMap, 42, false, part2)
+		after := GenerateRegexpString(rulesMap, 31, false, part2)
+
+		matchingString = append(matchingString, GenerateRegexpUpToNBeforeAndAfter(before, after, 5))
+	} else {
+		for _, part := range strings.Split(currentRule, " | ") {
+			stringPart := ""
+			for _, loopedRuleNumber := range StringsToInts(strings.Split(part, " ")) {
+				stringPart += GenerateRegexpString(rulesMap, loopedRuleNumber, false, part2)
+			}
+
+			matchingString = append(matchingString, stringPart)
+
 		}
-		matchingString = append(matchingString, stringPart)
 	}
 
-	return "^(" + strings.Join(matchingString, "|") + ")$"
+	result := "^(" + strings.Join(matchingString, "|") + ")$"
+	if includeStartAndEnd {
+		return result
+	} else {
+		return strings.Trim(result, "$^")
+	}
 
 }
 
@@ -67,25 +97,12 @@ func Parse(in []string) (map[int]string, []string) {
 	return ruleToStr, messages
 }
 
-func ReadFromStdIn() []string {
-	lines := []string{}
-	reader := bufio.NewReader(os.Stdin)
-
-read_loop:
-	for {
-		text, _ := reader.ReadString('\n')
-		if text == "go\n" {
-			break read_loop
-		}
-		lines = append(lines, strings.TrimSpace(text))
-	}
-
-	return lines
-}
-
 func StringsToInts(stringInputs []string) []int {
 	ints := []int{}
 	for _, str := range stringInputs {
+		if str == "" {
+			continue
+		}
 		i, err := strconv.Atoi(str)
 		if err != nil {
 			log.Fatal(err)
