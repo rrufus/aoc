f2df8c44bcc495253eefdc4379fd202e49231268
Rufus Russell <rufusrussell@gmail.com>
2020-12-20 10:40:29 +0000

    part 1

---
+++ 2020/20/index.js
@@ -0,0 +1,156 @@
+const { readFileSync } = require("fs");
+const input = readFileSync("input").toString();
+
+const sample = `Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###
+
+Tile 1951:
+#.##...##.
+#.####...#
+.....#..##
+#...######
+.##.#....#
+.###.#####
+###.##.##.
+.###....#.
+..#.#..#.#
+#...##.#..
+
+Tile 1171:
+####...##.
+#..##.#..#
+##.#..#.#.
+.###.####.
+..###.####
+.##....##.
+.#...####.
+#.##.####.
+####..#...
+.....##...
+
+Tile 1427:
+###.##.#..
+.#..#.##..
+.#.##.#..#
+#.#.#.##.#
+....#...##
+...##..##.
+...#.#####
+.#.####.#.
+..#..###.#
+..##.#..#.
+
+Tile 1489:
+##.#.#....
+..##...#..
+.##..##...
+..#...#...
+#####...#.
+#..#.#.#.#
+...#.#.#..
+##.#...##.
+..##.##.##
+###.##.#..
+
+Tile 2473:
+#....####.
+#..#.##...
+#.##..#...
+######.#.#
+.#...#.#.#
+.#########
+.###.#..#.
+########.#
+##...##.#.
+..###.#.#.
+
+Tile 2971:
+..#.#....#
+#...###...
+#.#.###...
+##.##..#..
+.#####..##
+.#..####.#
+#..#.#..#.
+..####.###
+..#.#.###.
+...#.#.#.#
+
+Tile 2729:
+...#.#.#.#
+####.#....
+..#.#.....
+....#..#.#
+.##..##.#.
+.#.####...
+####.#.#..
+##.####...
+##..#.##..
+#.##...##.
+
+Tile 3079:
+#.#.#####.
+.#..######
+..#.......
+######....
+####.#..#.
+.#...#.##.
+#.#####.##
+..#.###...
+..#.......
+..#.###...
+
+`;
+
+const reverse = (str = "") => str.split("").reverse().join("");
+
+const tiles = sample
+  .trim()
+  .split("\n\n")
+  .map((tile) => {
+    const allLines = tile.split("\n");
+
+    const id = parseInt(allLines[0].split(" ")[1].replace(":", ""));
+    const lines = allLines.filter((_, idx) => idx != 0);
+    const firstRow = lines[0];
+    const lastRow = lines[lines.length - 1];
+    const firstColumn = lines.map((l) => l[0]).join("");
+    const lastColumn = lines.map((l) => l[l.length - 1]).join("");
+
+    return {
+      id,
+      borders: [
+        firstRow,
+        reverse(firstRow),
+        lastRow,
+        reverse(lastRow),
+        firstColumn,
+        reverse(firstColumn),
+        lastColumn,
+        reverse(lastColumn),
+      ],
+    };
+  });
+
+const result = tiles.reduce((memo, tile) => {
+  const adjacent = tiles
+    .filter((t) => t.id !== tile.id)
+    .reduce(
+      (sum, t) =>
+        t.borders.some((b) => tile.borders.includes(b)) ? sum + 1 : sum,
+      0
+    );
+
+  return adjacent === 2 ? memo * tile.id : memo;
+}, 1);
+
+console.log({ result });
