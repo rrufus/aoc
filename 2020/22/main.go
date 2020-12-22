f4d0f8a48f81644e7fb56a7fb79240522a5af4ae
Rufus Russell <rufusrussell@gmail.com>
2020-12-22 11:21:27 +0100

    day 22 part 2

--- 2020/22/main.go
+++ 2020/22/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	in := ReadFromInput()
-	// in := ReadFromStdIn()
+type Turns [][][]int
 
-	p1, p2 := Parse(in)
+func (t Turns) isEqualToPrevious(d1, d2 []int) bool {
+	for _, previousTurn := range t {
+		prevDeck1, prevDeck2 := previousTurn[0], previousTurn[1]
 
-	fmt.Println("Part 1")
-	fmt.Println(CalculateScorePart1(PlayGamePart1(p1, p2)))
-
-	fmt.Println("Part 2")
-
+		if SliceEqual(d1, prevDeck1) && SliceEqual(d2, prevDeck2) {
+			return true
+		}
+	}
+	return false
 }
 
-func CalculateScorePart1(deck []int) (score int) {
+func main() {
+	in := ReadFromInput()
+	p1, p2 := Parse(in)
+
+	part1, _ := PlayGame(p1, p2, false)
+	fmt.Println("Part 1:", part1)
+	part2, _ := PlayGame(p1, p2, true)
+	fmt.Println("Part 2:", part2)
+}
+
+func CalculateScore(deck []int) (score int) {
 	for idx, card := range deck {
 		multiplier := len(deck) - idx
 		score += card * multiplier
@@ -30,29 +38,49 @@ func CalculateScorePart1(deck []int) (score int) {
 	return
 }
 
-func PlayGamePart1(d1, d2 []int) []int {
-
+func PlayGame(d1, d2 []int, part2 bool) (int, bool) {
+	turns := Turns{}
 	for len(d1) > 0 && len(d2) > 0 {
-		d1, d2 = Round(d1, d2)
+		if part2 {
+			if turns.isEqualToPrevious(d1, d2) {
+				return CalculateScore(d1), true
+			} else {
+				turns = append(turns, [][]int{d1, d2})
+			}
+		}
+		d1, d2 = Round(d1, d2, part2)
 	}
 
 	if len(d1) > 0 {
-		return d1
-	} else {
-		return d2
+		return CalculateScore(d1), true
 	}
+	return CalculateScore(d2), false
 }
 
-func Round(d1, d2 []int) ([]int, []int) {
+func Round(d1, d2 []int, part2 bool) ([]int, []int) {
 	cardd1, restDeck1 := d1[0], d1[1:]
 	cardd2, restDeck2 := d2[0], d2[1:]
 
-	if cardd1 > cardd2 {
+	if part2 && cardd1 <= len(restDeck1) && cardd2 <= len(restDeck2) {
+		// must copy or we get memory overwrites
+		copyDeck1 := make([]int, cardd1)
+		copyDeck2 := make([]int, cardd2)
+		copy(copyDeck1, restDeck1)
+		copy(copyDeck2, restDeck2)
+
+		_, d1Wins := PlayGame(copyDeck1, copyDeck2, part2)
+
+		if d1Wins {
+			return append(restDeck1, cardd1, cardd2), restDeck2
+		} else {
+			return restDeck1, append(restDeck2, cardd2, cardd1)
+		}
+
+	} else if cardd1 > cardd2 {
 		return append(restDeck1, cardd1, cardd2), restDeck2
 	} else {
 		return restDeck1, append(restDeck2, cardd2, cardd1)
 	}
-
 }
 
 func Parse(in []string) ([]int, []int) {
@@ -77,32 +105,19 @@ func Parse(in []string) ([]int, []int) {
 	return player1Deck, player2Deck
 }
 
-func ReadFromStdIn() []string {
-	lines := []string{}
-	reader := bufio.NewReader(os.Stdin)
-
-read_loop:
-	for {
-		text, _ := reader.ReadString('\n')
-		if text == "go\n" {
-			break read_loop
-		}
-		lines = append(lines, strings.TrimSpace(text))
-	}
-
-	return lines
-}
-
-func StringsToInts(stringInputs []string) []int {
-	ints := []int{}
-	for _, str := range stringInputs {
-		i, _ := strconv.Atoi(str)
-		ints = append(ints, i)
-	}
-	return ints
-}
-
 func ReadFromInput() []string {
 	bytes, _ := ioutil.ReadFile("input")
 	return strings.Split(strings.TrimSpace(string(bytes)), "\n")
+}
+
+func SliceEqual(s1, s2 []int) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for idx, item := range s1 {
+		if s2[idx] != item {
+			return false
+		}
+	}
+	return true
 }
