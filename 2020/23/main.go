4ce28714b99dee05a4d7325d2fe9ae96fd2fea61
Rufus Russell <rufusrussell@gmail.com>
2020-12-23 07:30:15 +0100

    part 2

--- 2020/23/main.go
+++ 2020/23/main.go
@@ -14,45 +14,60 @@ type Cup struct {
 }
 
 func main() {
-	in := ReadFromInput()
-	cupInts := StringsToInts(strings.Split(in[0], ""))
+	cupInts := StringsToInts(strings.Split(ReadFromInput()[0], ""))
 
-	cups := make([]*Cup, len(cupInts))
-	for idx, _ := range cups {
+	cupsP1, lookupP1 := SetupCups(cupInts, len(cupInts))
+
+	PlayCups(cupsP1, lookupP1, 100, false)
+
+	fmt.Printf("Part 1: ")
+	cursor := lookupP1[1].next
+	for i := 0; i < len(cupsP1)-1; i++ {
+		fmt.Printf("%v", cursor.val)
+		cursor = cursor.next
+	}
+	fmt.Println("")
+
+	cupsP2, lookupP2 := SetupCups(cupInts, 1000000)
+
+	PlayCups(cupsP2, lookupP2, 10000000, false)
+
+	fmt.Println("Part 2:", lookupP2[1].next.val*lookupP2[1].next.next.val)
+}
+
+func SetupCups(cupInts []int, n int) ([]*Cup, map[int]*Cup) {
+	valsToPtrs := map[int]*Cup{}
+	cups := make([]*Cup, n)
+	for idx := range cups {
 		cups[idx] = &Cup{}
 	}
-	var cupContainingOne *Cup
-	for idx, cupInt := range cupInts {
-		cups[idx].val = cupInt
-		cups[idx].prev = cups[(idx-1+len(cupInts))%len(cupInts)]
-		cups[idx].next = cups[(idx+1)%len(cupInts)]
-		if cupInt == 1 {
-			cupContainingOne = cups[idx]
+
+	for idx, cup := range cups {
+		cupInt := idx + 1
+		if idx < len(cupInts) {
+			cupInt = cupInts[idx]
 		}
+		valsToPtrs[cupInt] = cup
+		cup.val = cupInt
+		cup.prev = cups[(idx-1+len(cups))%len(cups)]
+		cup.next = cups[(idx+1)%len(cups)]
 	}
 
-	lowest := cupInts[0]
-	highest := cupInts[0]
-	for _, cupInt := range cupInts {
-		if cupInt < lowest {
-			lowest = cupInt
-		}
-		if cupInt > highest {
-			highest = cupInt
-		}
-	}
+	return cups, valsToPtrs
+}
 
+func PlayCups(cups []*Cup, lookup map[int]*Cup, rounds int, print bool) {
 	currentCup := cups[0]
-
-	fmt.Println(lowest, highest)
-
-	fmt.Println("Part 1")
-	for i := 0; i < 100; i++ {
+	lowest := 1
+	highest := len(cups)
+	for i := 0; i < rounds; i++ {
 		isolatedCupsStart := currentCup.next
 		isolatedCupsEnd := currentCup.next.next.next
 
-		fmt.Printf("-- move %v --\n", i+1)
-		fmt.Println("Pick up:", isolatedCupsStart.val, isolatedCupsStart.next.val, isolatedCupsEnd.val)
+		if print {
+			fmt.Printf("-- move %v --\n", i+1)
+			fmt.Println("pick up:", isolatedCupsStart.val, isolatedCupsStart.next.val, isolatedCupsEnd.val)
+		}
 
 		currentCup.next = currentCup.next.next.next.next
 		currentCup.next.prev = currentCup
@@ -60,17 +75,17 @@ func main() {
 		nLookingFor := currentCup.val - 1
 		for nLookingFor == isolatedCupsStart.val ||
 			nLookingFor == isolatedCupsStart.next.val ||
-			nLookingFor == isolatedCupsEnd.val || nLookingFor < lowest {
+			nLookingFor == isolatedCupsEnd.val ||
+			nLookingFor < lowest {
 			nLookingFor--
 			if nLookingFor < lowest {
 				nLookingFor = highest
 			}
 		}
-		destination := currentCup.next
-		for destination.val != nLookingFor {
-			destination = destination.next
+		destination := lookup[nLookingFor]
+		if print {
+			fmt.Println("destination:", destination.val)
 		}
-		fmt.Println("destination:", destination.val)
 		oldNext := destination.next
 		isolatedCupsEnd.next = oldNext
 		oldNext.prev = isolatedCupsEnd
@@ -79,16 +94,6 @@ func main() {
 
 		currentCup = currentCup.next
 	}
-	fmt.Println("-- final --")
-	cursor := cupContainingOne.next
-	for i := 0; i < len(cups)-1; i++ {
-		fmt.Printf("%v", cursor.val)
-		cursor = cursor.next
-	}
-	fmt.Println("\n")
-
-	fmt.Println("Part 2")
-
 }
 
 func StringsToInts(stringInputs []string) []int {
